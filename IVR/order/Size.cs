5c406b1ecf35c1fb0de598333b1cc519269e6a8f
DanielosVK <danielkrause2h@op.pl>
2016-11-23 20:38:34 +0100

    Update Size.cs

--- IVR/order/Size.cs
+++ IVR/order/Size.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using IVR.prompts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,35 @@ using System.Threading.Tasks;
 
 namespace IVR.order
 {
-    class Size
+    class Size : IItem
     {
         // prompt
+        Prompt prompt;
+        private string name;
 
+
+
+        public Size(string name, Prompt p)
+        {
+            this.name = name;
+            this.prompt = p;
+        }
+
+        public Size(string name)
+        {
+            this.name = name;
+
+        }
+
+        public List<Prompt> GetPrompts()
+        {
+            return new List<Prompt>() { prompt };
+        }
+        public string GetName()
+        {
+            return name;
+        }
 
     }
+
 }
